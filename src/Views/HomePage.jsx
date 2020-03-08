ab9c539a59c95832b04da982c3ea4ac6b615211b
seanpara <spara77@gmail.com>
2020-03-08 16:51:19 -0400

    changes to homepage

--- src/Views/HomePage.jsx
+++ src/Views/HomePage.jsx
@@ -9,22 +9,26 @@ import { siteImages } from "../images";
 const useStyles = makeStyles(theme => ({
   paper: {
     padding: "5%",
+    margin: "2%",
     background: theme.palette.secondary.dark,
     color: "white",
-    width: "100%",
+    width: "80%",
     height: "100%",
     textAlign: "center",
     display: "flex",
     flexDirection: "column",
+  },
+  carouselPaper: {
+    background: "black",
   },
 }));
 export default () => {
   const classes = useStyles();
   const renderImages = () =>
     siteImages.map(imageSrc => (
-      <div key={imageSrc}>
-        <img style={{ width: "60%", height: "70%" }} src={imageSrc} alt="" />
-      </div>
+      <Paper className={classes.carouselPaper} key={imageSrc}>
+        <img style={{ width: "60%", height: "auto" }} src={imageSrc} alt="" />
+      </Paper>
     ));
   return (
     <div
@@ -33,10 +37,12 @@ export default () => {
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
+        background: "#1976d2",
       }}
     >
-      <Carousel>{renderImages()}</Carousel>
+      <Carousel showThumbs={false}>{renderImages()}</Carousel>
       <Paper className={classes.paper}>
+        <p>Hi fergas </p>
         <p>41 Varick </p>
         <p>fbaumann@bennington.edu</p>
       </Paper>
