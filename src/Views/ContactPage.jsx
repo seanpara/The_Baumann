49b9efdb783827f564ef3a993676a575ae7ecf0b
seanpara <spara77@gmail.com>
2020-05-31 23:03:32 -0400

    moves contact submit to opimstic rendering with error handling

--- src/Views/ContactPage.jsx
+++ src/Views/ContactPage.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -20,8 +20,6 @@ const RESET_FORM = 'RESET_FORM';
 const formReducer = (state, { type, payload }) => {
   switch (type) {
     case SET_FIELD:
-      console.log(payload);
-
       return { ...state, [payload.field]: payload.value };
     case RESET_FORM:
       return intialFormState;
@@ -34,6 +32,8 @@ const ContactPage = () => {
     formReducer,
     intialFormState,
   );
+  const [errorMessage, setErrorMessage] = useState('');
+  const classes = useHomePageStyles();
 
   const handleFieldChange = ({ target: { value } }, field) =>
     // @ts-ignore
@@ -42,29 +42,60 @@ const ContactPage = () => {
       payload: { field, value },
     });
 
-  const handleFormSubmit = () => {
-    fetch('https://us-central1-baumann-firebase.cloudfunctions.net/sendMail', {
-      method: 'POST',
-      body: JSON.stringify(formState),
-    })
-      .then((res) => {
-        if (res.status === 200) {
-          return res.json();
-        } else {
-          throw new Error(res.json().error);
-        }
-      })
-      .then((r) => {
-        console.log(r);
-        // @ts-ignore
-        dispatchFormAction({ type: RESET_FORM });
-      });
+  const handleFormSubmit = async () => {
+    // @ts-ignore
+    dispatchFormAction({ type: RESET_FORM });
+
+    const res = await fetch(
+      'https://us-central1-baumann-firebase.cloudfunctions.net/sendMail',
+      {
+        method: 'POST',
+        body: JSON.stringify(formState),
+      },
+    );
+    const parsedRes = res.json();
+    if (res.status !== 200) {
+      const errorMessage = parsedRes.error;
+      setErrorMessage(errorMessage);
+    } else {
+      console.log(parsedRes.message);
+    }
   };
 
-  const classes = useHomePageStyles();
-  const { name, email, website, source, message } = formState;
-  console.log('Form state:', formState);
-
+  const renderForm = () =>
+    Object.keys(formState).map((stateKey) =>
+      stateKey !== 'source' ? (
+        <TextField
+          key={stateKey}
+          required={stateKey === 'name' || stateKey === 'email'}
+          label={stateKey.charAt(0).toUpperCase() + stateKey.slice(1)}
+          value={formState[stateKey]}
+          onChange={(e) => handleFieldChange(e, stateKey)}
+          error={Boolean(errorMessage)}
+          helperText={errorMessage}
+        />
+      ) : (
+        <div
+          key={stateKey}
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            height: '20%',
+            justifyContent: 'space-apart',
+          }}
+        >
+          <FormHelperText>How Did you here about us?</FormHelperText>
+          <Select
+            value={formState[stateKey]}
+            onChange={(e) => handleFieldChange(e, stateKey)}
+          >
+            <MenuItem value={'friend'}>Friend</MenuItem>
+            <MenuItem value={'online'}>Online</MenuItem>
+          </Select>
+        </div>
+      ),
+    );
+  const { name, email } = formState;
   return (
     <div className={classes.root} style={{ height: '100vh' }}>
       <div
@@ -75,48 +106,7 @@ const ContactPage = () => {
           justifyContent: 'space-between',
         }}
       >
-        <TextField
-          required
-          label="Name"
-          value={name}
-          onChange={(e) => handleFieldChange(e, 'name')}
-        />
-        <TextField
-          required
-          label="Email"
-          value={email}
-          onChange={(e) => handleFieldChange(e, 'email')}
-        />
-        <TextField
-          label="Website"
-          value={website}
-          onChange={(e) => handleFieldChange(e, 'website')}
-        />
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            height: '20%',
-            justifyContent: 'space-apart',
-          }}
-        >
-          <FormHelperText>How Did you here about us?</FormHelperText>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={source}
-            onChange={(e) => handleFieldChange(e, 'source')}
-          >
-            <MenuItem value={'friend'}>Friend</MenuItem>
-            <MenuItem value={'online'}>Online</MenuItem>
-          </Select>
-        </div>
-
-        <TextField
-          label="Message"
-          value={message}
-          onChange={(e) => handleFieldChange(e, 'message')}
-        />
+        {renderForm()}
         <Button
           onClick={handleFormSubmit}
           variant="contained"
