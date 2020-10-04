b9b0ebf582614f51ea4f03e24e32ee2fdd427566
seanpara <spara77@gmail.com>
2020-10-04 17:47:25 -0400

    begins revising contact page

--- src/Views/ContactPage.jsx
+++ src/Views/ContactPage.jsx
@@ -4,6 +4,7 @@ import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
 
 import { useHomePageStyles } from '../styles';
 
@@ -17,16 +18,20 @@ const intialFormState = {
 
 const SET_FIELD = 'SET_FIELD';
 const RESET_FORM = 'RESET_FORM';
-const formReducer = (state, { type, payload }) => {
+const formReducer = (
+  state,
+  { type, payload: { field = '', value = '' } = {} },
+) => {
   switch (type) {
     case SET_FIELD:
-      return { ...state, [payload.field]: payload.value };
+      return { ...state, [field]: value };
     case RESET_FORM:
       return intialFormState;
     default:
       return state;
   }
 };
+
 const ContactPage = () => {
   const [formState, dispatchFormAction] = useReducer(
     formReducer,
@@ -53,10 +58,9 @@ const ContactPage = () => {
         body: JSON.stringify(formState),
       },
     );
-    const parsedRes = res.json();
-    if (res.status !== 200) {
-      const errorMessage = parsedRes.error;
-      setErrorMessage(errorMessage);
+    const { status, error } = res.json();
+    if (status !== 200) {
+      setErrorMessage(error);
     }
   };
 
@@ -82,7 +86,7 @@ const ContactPage = () => {
             justifyContent: 'space-apart',
           }}
         >
-          <FormHelperText>How Did you here about us?</FormHelperText>
+          <FormHelperText>How Did you hear about us?</FormHelperText>
           <Select
             value={formState[stateKey]}
             onChange={(e) => handleFieldChange(e, stateKey)}
@@ -95,25 +99,43 @@ const ContactPage = () => {
     );
   const { name, email } = formState;
   return (
-    <div className={classes.root} style={{ height: '100vh' }}>
-      <div
+    <div
+      className={classes.root}
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+      }}
+    >
+      <h1
+        style={{
+          fontFamily: 'serif',
+        }}
+      >
+        Please contact us with questions or if you would like to get involved!
+      </h1>
+      <Paper
+        elevation={4}
         style={{
           display: 'flex',
           flexDirection: 'column',
           height: '50%',
+          width: '60%',
           justifyContent: 'space-between',
+          backgroundColor: 'grey',
         }}
       >
         {renderForm()}
         <Button
           onClick={handleFormSubmit}
           variant="contained"
-          color="primary"
+          style={{ backgroundColor: '#0d47a1' }}
           disabled={!name || !email}
         >
           Submit
         </Button>
-      </div>
+      </Paper>
     </div>
   );
 };
