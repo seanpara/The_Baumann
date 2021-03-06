9b667dee3cb29c89886a8bd0896d2b1431c97a96
Sean Para <seanpara@Seans-MacBook-Pro.local>
2021-03-06 17:25:23 -0500

    changes text field color to white in contact page

--- src/Views/ContactPage.tsx
+++ src/Views/ContactPage.tsx
@@ -1,40 +1,40 @@
-import React, { useReducer, useState, useEffect } from 'react';
-import { useRecoilState } from 'recoil';
+import React, { useReducer, useState, useEffect } from "react";
+import { useRecoilState } from "recoil";
 
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
-import { useHomePageStyles } from '../styles';
-import { contactState } from '../atoms';
+import { useHomePageStyles } from "../styles";
+import { contactState } from "../atoms";
 
 /// typing will be corrected when this is refactored with the new mock ups
 const bookingFields = {
-  name: '',
-  email: '',
-  phoneNumber: '',
-  description: '',
-  date: '',
+  name: "",
+  email: "",
+  phoneNumber: "",
+  description: "",
+  date: "",
 };
 
 const generalContactFields = {
-  name: '',
-  email: '',
-  message: '',
+  name: "",
+  email: "",
+  message: "",
 };
 
 const fieldNameToPlaceholderTextMapping: { [key: string]: string } = {
-  name: 'Name',
-  email: 'Email',
-  phoneNumber: 'Phone Number',
-  description: 'Description of Booking',
-  date: 'Desired Date(s)',
-  websiteOrSocialMedia: 'Website or Social Media Link',
+  name: "Name",
+  email: "Email",
+  phoneNumber: "Phone Number",
+  description: "Description of Booking",
+  date: "Desired Date(s)",
+  websiteOrSocialMedia: "Website or Social Media Link",
 };
 
-const SET_FIELD = 'SET_FIELD';
-const SET_ALL_FIELDS = 'SET_ALL_FIELDS';
+const SET_FIELD = "SET_FIELD";
+const SET_ALL_FIELDS = "SET_ALL_FIELDS";
 
-const RESET_FORM = 'RESET_FORM';
+const RESET_FORM = "RESET_FORM";
 
 interface FormState {
   [key: string]: string | number | null;
@@ -45,7 +45,7 @@ const ContactPage = () => {
     (state: FormState, { type, payload }: { type: string; payload?: any }) => {
       switch (type) {
         case SET_FIELD: {
-          const { field = '', value = '' } = payload;
+          const { field = "", value = "" } = payload;
           return { ...state, [field]: value };
         }
         case SET_ALL_FIELDS: {
@@ -57,9 +57,9 @@ const ContactPage = () => {
           return state;
       }
     },
-    generalContactFields,
+    generalContactFields
   );
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
 
   const classes = useHomePageStyles();
 
@@ -70,14 +70,14 @@ const ContactPage = () => {
       dispatchFormAction({
         type: SET_ALL_FIELDS,
         payload:
-          contactType === 'general' ? generalContactFields : bookingFields,
+          contactType === "general" ? generalContactFields : bookingFields,
       });
     }
   }, [contactType]);
 
   const makeASentence = (word: string): string =>
     word.replace(/^[a-z]|[A-Z]/g, (v, i) =>
-      i === 0 ? v.toUpperCase() : ' ' + v.toLowerCase(),
+      i === 0 ? v.toUpperCase() : " " + v.toLowerCase()
     );
 
   const handleFieldChange = ({ target: { value } }: any, field: any) =>
@@ -90,11 +90,11 @@ const ContactPage = () => {
     dispatchFormAction({ type: RESET_FORM });
 
     const { status, error } = await fetch(
-      'https://us-central1-baumann-firebase.cloudfunctions.net/sendMail',
+      "https://us-central1-baumann-firebase.cloudfunctions.net/sendMail",
       {
-        method: 'POST',
+        method: "POST",
         body: JSON.stringify(formState),
-      },
+      }
     ).then((r): Promise<{ status: number; error: string }> => r.json());
 
     if (status !== 200) {
@@ -105,9 +105,10 @@ const ContactPage = () => {
   const renderFormField = (stateKey: string): JSX.Element => (
     <TextField
       key={stateKey}
-      required={stateKey === 'name' || stateKey === 'email'}
+      required={stateKey === "name" || stateKey === "email"}
       style={{
-        width: '80%',
+        width: "80%",
+        backgroundColor: "white",
       }}
       variant="filled"
       label={
@@ -127,14 +128,14 @@ const ContactPage = () => {
   return (
     <div className={classes.contactPage}>
       <h1>
-        {contactType === 'booking' ? 'Book with Us!' : 'Send Us a Message!'}
+        {contactType === "booking" ? "Book with Us!" : "Send Us a Message!"}
       </h1>
       {renderForm()}
       <Button
         disabled={!name || !email}
         onClick={handleFormSubmit}
         variant="contained"
-        style={{ width: '40%', marginTop: '5%' }}
+        style={{ width: "40%", marginTop: "5%" }}
       >
         Submit
       </Button>
