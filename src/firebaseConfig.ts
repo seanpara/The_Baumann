37d63e757e55e4b1ee05d0575e03f0944cfe1e56
Sean Para <seanpara@Seans-MacBook-Pro.local>
2021-04-11 14:57:04 -0400

    env setup issues

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,26 +1,10 @@
-import localConfig from "./localConfig";
-
-const isProduction = process.env.NODE_ENV === "production";
-
 export const firebaseConfig = {
-  apiKey: isProduction ? process.env.REACT_APP_API_KEY : localConfig.apiKey,
-  authDomain: isProduction
-    ? process.env.REACT_APP_AUTH_DOMAIN
-    : localConfig.authDomain,
-  databaseURL: isProduction
-    ? process.env.REACT_APP_DATABASE_URL
-    : localConfig.databaseURL,
-  projectId: isProduction
-    ? process.env.REACT_APP_PROJECT_ID
-    : localConfig.projectId,
-  storageBucket: isProduction
-    ? process.env.REACT_APP_STORAGE_BUCKET
-    : localConfig.storageBucket,
-  messagingSenderId: isProduction
-    ? process.env.REACT_APP_MESSAGING_SENDER_ID
-    : localConfig.messagingSenderId,
-  appId: isProduction ? process.env.REACT_APP_APP_ID : localConfig.appId,
-  measurementId: isProduction
-    ? process.env.REACT_APP_MEASUREMENT_ID
-    : localConfig.measurementId,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
